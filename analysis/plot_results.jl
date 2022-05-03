2679cfdf325064827507f5ce89996d8ded3cf2b1
Erich Essmann <essmanne@gmail.com>
2022-05-03 10:18:06 +0200

    Remove sqlite

--- analysis/plot_results.jl
+++ analysis/plot_results.jl
@@ -4,42 +4,60 @@ plot_results:
 - Author: erich
 - Date: 2021-04-09
 =#
-using CSV, DataFrames, Plots, SQLite
-db = SQLite.DB("analysis/data.sqlite")
+using CSV, DataFrames, Plots, SQLite, BenchmarkTools, LinearAlgebra
 
-sym_data =
-    CSV.File(
-        "cmake-build-release-aocc/src/csv_symm_results_2021-04-15-15-16-44.csv",
+function generate_plots(fp,pl_title, plot_name)
+    sym_data = CSV.File(
+        fp,
         normalizenames = true,
     ) |> DataFrame
 
-labels = ["Eigen" "Blaze" "GSL" "xtensor"]
-plt1 = plot(
-    sym_data.size,
-    [sym_data.Eigen sym_data.Blaze sym_data.GSL sym_data.Xtensor],
-    plot_title = "Symmetric Matrix Eigenvalues",
-    xlabel = "Matrix size, N",
-    ylabel = "Time elasped, μs",
-    yaxis = :log10,
-    label = labels,
-    legend = :topleft,
-)
-savefig(plt1, "symm_results.pdf")
+    labels = ["Eigen" "Blaze" "GSL" "xtensor"]
+    plt = plot(
+        sym_data.size,
+        [sym_data.Eigen sym_data.Blaze sym_data.GSL sym_data.Xtensor],
+        plot_title = pl_title,
+        xlabel = "Matrix size, N",
+        ylabel = "Time elasped, μs",
+        yaxis = :log10,
+        label = labels,
+        legend = :topleft)
+    savefig(plt, plot_name)
+    
+end
 
-asym_data =
-    CSV.File(
-        "cmake-build-release-aocc/src/csv_asymm_results_2021-04-15-15-22-21.csv",
+function run_Julia_sym(fp)
+    sym_data = CSV.File(
+        fp,
         normalizenames = true,
     ) |> DataFrame
 
-plt2 = plot(
-    asym_data.size,
-    [asym_data.Eigen asym_data.Blaze asym_data.GSL asym_data.Xtensor],
-    plot_title = "Asymmetric Matrix Eigenvalues",
-    xlabel = "Matrix size, N",
-    ylabel = "Time elasped, μs",
-    yaxis = :log10,
-    label = labels,
-    legend = :topleft,
-)
-savefig(plt2, "asymm_results.pdf")
+    function sym_invert(n::Int, alpha::Float64)
+        julia_pie = ones(n, n) + alpha * I
+        function inner_loop(mat::Matrix)
+            for i in 1:32 
+                inv(mat)
+            end
+            return mat
+        end
+        
+        res = @benchmark inner_loop($julia_pie)
+        return mean(res).time
+    end
+
+    times = Vector{Float64}
+    for i in 2:256
+        append!(times, sym_invert(i, 0.4))
+    end
+
+    sym_data[:Julia] = times
+
+
+end
+
+
+function main(alpha::Float64)
+
+    run_Julia_sym("cmake-build-release/src/csv_symm_results_2022-02-10-11-57-03.csv")
+    
+end
